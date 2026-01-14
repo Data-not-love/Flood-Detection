d76eef84338a9a8ca966d7e8269563d417c5b9d7
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2026-01-14 08:40:09 +0700

    First commit to test

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+from logs.log_generator import log_generator
+import logging
+
+os.makedirs("logs", exist_ok=True)
+logger_gen = log_generator("LOG_FILE_PATH")
+logger_gen.log_config()
+
+logging.info("This is an INFO log")
+logging.warning("This is a WARNING log")
+logging.error("This is an ERROR log")
+logging.critical("This is a CRITICAL log")
+logging.debug("This is a DEBUG log")
+logging.fatal("This is a FATAL log")
+print("Log file path:", logger_gen.get_log_file_path())
